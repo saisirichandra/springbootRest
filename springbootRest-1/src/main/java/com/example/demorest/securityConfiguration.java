0cae4c2028f88d0c6ba0b2446fb2d5e2d757abea
saisirichandra <sirichandrapodugu@gmail.com>
2021-05-10 12:38:43 +0530

    Authorization cofig

--- springbootRest-1/src/main/java/com/example/demorest/securityConfiguration.java
+++ springbootRest-1/src/main/java/com/example/demorest/securityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.demorest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -16,7 +17,11 @@ public class securityConfiguration extends WebSecurityConfigurerAdapter {
 		auth.inMemoryAuthentication()
 		.withUser("siri")
 		.password("ihlet")
-		.roles("USER");
+		.roles("user")
+		.and()
+		.withUser("chandra")
+		.password("akshay")
+		.roles("ADMIN");
 		//super.configure(auth);
 		
 	}
@@ -24,5 +29,14 @@ public class securityConfiguration extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
 		
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/**")//match for all the paths
+		.hasRole("ADMIN")
+		//.hasAnyRole("_","_")
+		.and()
+		.formLogin();
 	}
 }
